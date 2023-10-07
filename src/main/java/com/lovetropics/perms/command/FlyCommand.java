f26629b789bf951fa37d3feef2520b43322ae22e
Gegy <gegy.dev@gmail.com>
2023-10-07 18:05:21 +0200

    Fix #3: always allow flight in creative, stop flying when no longer allowed to fly

--- src/main/java/com/lovetropics/perms/command/FlyCommand.java
+++ src/main/java/com/lovetropics/perms/command/FlyCommand.java
@@ -16,25 +16,27 @@ public class FlyCommand {
 	public static void register(CommandDispatcher<CommandSourceStack> dispatcher) {
 		dispatcher.register(literal("fly")
 				.requires(ctx -> ctx.hasPermission(Commands.LEVEL_GAMEMASTERS))
-				.then(literal("enable").executes(context -> updateFly(context, true)))
-				.then(literal("disable").executes(context -> updateFly(context, false)))
-				.executes(ctx -> {
-					ServerPlayer player = ctx.getSource().getPlayerOrException();
-					player.getAbilities().mayfly = !player.getAbilities().mayfly;
-					player.onUpdateAbilities();
-					return Command.SINGLE_SUCCESS;
-				}));
+				.then(literal("enable").executes(context -> setFlight(context, true)))
+				.then(literal("disable").executes(context -> setFlight(context, false)))
+				.executes(FlyCommand::toggleFlight));
 	}
 
-	private static int updateFly(CommandContext<CommandSourceStack> context, boolean canFly) throws CommandSyntaxException {
-		ServerPlayer player = context.getSource().getPlayerOrException();
-		Abilities abilities = player.getAbilities();
-		canFly |= player.isCreative();
-		abilities.mayfly = canFly;
-		if (!canFly) {
-			abilities.flying = false;
-		}
-		player.onUpdateAbilities();
+	private static int toggleFlight(final CommandContext<CommandSourceStack> ctx) throws CommandSyntaxException {
+		ServerPlayer player = ctx.getSource().getPlayerOrException();
+		setFlight(player, !player.getAbilities().mayfly);
 		return Command.SINGLE_SUCCESS;
+	}
+
+	private static int setFlight(CommandContext<CommandSourceStack> context, boolean canFly) throws CommandSyntaxException {
+		ServerPlayer player = context.getSource().getPlayerOrException();
+		setFlight(player, canFly);
+		return Command.SINGLE_SUCCESS;
+	}
+
+	private static void setFlight(final ServerPlayer player, final boolean canFly) {
+		Abilities abilities = player.getAbilities();
+		abilities.mayfly = canFly || player.isCreative();
+		abilities.flying &= abilities.mayfly;
+		player.onUpdateAbilities();
 	}
 }
